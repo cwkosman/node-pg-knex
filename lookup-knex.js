bdd9e41806d7d554e8f2e7cfa46c896418d88964
cwkosman <cwkosman@gmail.com>
2017-02-28 22:50:45 +0000

    Basic knex setup and test query

---
+++ lookup-knex.js
@@ -0,0 +1,14 @@
+const settings = require("./settings-knex.json");
+const knex = require('knex')({
+  client: 'pg',
+  connection: settings
+});
+
+function queryName() {
+  knex.select().from('famous_people').then(function(rows) {
+    console.log(rows);
+    knex.destroy();
+  });
+}
+
+queryName();
