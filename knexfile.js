92248c7ce2bbbe62a3b6d5acb731947591d28ca7
cwkosman <cwkosman@gmail.com>
2017-03-01 01:45:49 +0000

    Migration: Add milestones table

---
+++ knexfile.js
@@ -0,0 +1,42 @@
+const devSettings = require("./settings.json");
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: devSettings
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
