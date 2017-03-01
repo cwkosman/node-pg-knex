0a4869c5f77ca1fbe03dfad9614175bb4e4bd86c
cwkosman <cwkosman@gmail.com>
2017-03-01 00:23:28 +0000

    Knex select

--- lookup-knex.js
+++ lookup-knex.js
@@ -1,14 +1,21 @@
-const settings = require("./settings.json");
-const knex = require('knex')({
-  client: 'pg',
-  connection: settings
-});
+const db = require("./lib/db-knex.js");
 
-function queryName() {
-  knex.select().from('famous_people').then(function(rows) {
-    console.log(rows);
-    knex.destroy();
-  });
+function queryName(query, callback) {
+  console.log(`Searching for "${query}"...`);
+  db.select()
+  .from('famous_people')
+  .where('first_name', 'LIKE', `%${query}%`)
+  .orWhere('last_name', 'LIKE', `%${query}%`)
+  .then(result => callback(result));
+  db.destroy();
 }
 
-queryName();
+queryName(process.argv[2], ((result) => {
+  if (!result.length) {
+    return console.log("No results found.");
+  }
+  console.log(`Found ${result.length} person(s) by the name '${process.argv[2]}'`);
+  result.forEach((match) => {
+    console.log(` - ${match.id}: ${match.first_name} ${match.last_name}, born ${match.birthdate.getFullYear()}-${match.birthdate.getMonth()}-${match.birthdate.getDate()}`);
+  });
+}));
