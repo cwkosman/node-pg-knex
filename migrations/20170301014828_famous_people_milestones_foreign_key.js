e0af1954d5fc35914bd107932b078a8522cd2ee0
cwkosman <cwkosman@gmail.com>
2017-03-01 03:36:33 +0000

    Migration: Add FK constraint milestones_famous_person_id

---
+++ migrations/20170301014828_famous_people_milestones_foreign_key.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table("milestones", (table) => {
+    table.integer('famous_person_id').unsigned();
+    table.foreign('famous_person_id').references('famous_people.id');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table("milestones", (table) => {
+    table.dropForeign('famous_person_id').references('famous_people.id');
+    table.dropColumn('famous_person_id');
+  });
+};
